9a6b065a15d5f3c4eb1ecd4fd284f23c02d36372
Satish2021N <saishdevilliers17@gmail.com>
2023-07-15 14:47:46 +0545

    Wrapper Component Added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -6,7 +6,8 @@ function ExpenseItem({date, title, amount}) {
 
   return (
     <div className="expense-item">
-      <ExpenseDate date={date}/>
+      <ExpenseDate date={date}></ExpenseDate>
+
       <div className="expense-item__description">
         <h2> {title}</h2>
         <div className="expense-item__price"> ${amount}</div>
